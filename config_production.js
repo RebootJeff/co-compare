013e21861cdb377be176e16a26400774fa94c676
Jeff Lee <leejeffc@gmail.com>
2013-11-16 11:23:13 -0800

    Adds pool settings to prevent leaking connections

--- config_production.js
+++ config_production.js
@@ -3,13 +3,16 @@ module.exports = {
   port: process.env.PORT,
   db: {
     name: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    port: process.env.DB_PORT,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    dialect: process.env.DB_DIALECT,
-    protocol: process.env.DB_PROTOCOL,
-    logging: console.log
+    options: {
+      host: process.env.DB_HOSTNAME,
+      port: process.env.DB_PORT,
+      dialect: process.env.DB_DIALECT,
+      protocol: process.env.DB_PROTOCOL,
+      logging: console.log,
+      pool: { maxConnections: 10, maxIdleTime: 30000 }
+    }
   },
   fb: {
     APP_KEY: process.env.FB_APP_KEY,
