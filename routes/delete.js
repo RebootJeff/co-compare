44714cf7646da248e617c4b7e63269cdf9722eb5
Jeff Lee <leejeffc@gmail.com>
2013-11-17 23:57:25 -0800

    Changed deletion technique from QueryChainer to chaining promises

--- routes/delete.js
+++ routes/delete.js
@@ -7,7 +7,6 @@ exports.removeComparison = function(req, res) {
     hash = req.params.hash,
     userId = parseInt(req.params.userId, 10),
     comparisonId;
-    var chainer = new require('sequelize').Utils.QueryChainer();
 
 // TODO: Refactor to take better advantage of associations?
   sequelize
@@ -15,47 +14,28 @@ exports.removeComparison = function(req, res) {
     .then(function(){
       return models.Comparison.find({ where: {hash: hash, UserId: userId} });
     })
-    
-    // .then(function(comparison){
-    //   comparisonId = comparison.id;
-    //   models.Comparison.destroy({ id: comparisonId, UserId: userId });
-    // })
-    // .then(function(){
-    //   models.Subject.destroy({ ComparisonId: comparisonId });
-    // })
-    // .then(function(){
-    //   models.Criterion.destroy({ ComparisonId: comparisonId });
-    // })
-    // .then(function(){
-    //   models.Score.destroy({ ComparisonId: comparisonId });
-    // })
-    // .then(function(){
-    //   models.Vote.destroy({ ComparisonId: comparisonId });
-    // })
-    // .then(
-    //   function(){
-    //     res.send(204);
-    //   },
-    //   function(error){
-    //     res.send(500, error);
-    //   }
-    // );
-
     .then(function(comparison){
-      console.log(comparison.id);
       comparisonId = comparison.id;
-      chainer
-        .add(models.Comparison.destroy({ id: comparisonId, UserId: userId }))
-        .add(models.Subject.destroy({ ComparisonId: comparisonId }))
-        .add(models.Criterion.destroy({ ComparisonId: comparisonId }))
-        .add(models.Score.destroy({ ComparisonId: comparisonId }))
-        .add(models.Vote.destroy({ ComparisonId: comparisonId }))
-        .run()
-        .success(function(){
-          res.send(204);
-        })
-        .error(function(err){
-          res.send(500, 'Failed to delete comparison.');
-        });
-    });
+      models.Comparison.destroy({ id: comparisonId, UserId: userId });
+    })
+    .then(function(){
+      models.Subject.destroy({ ComparisonId: comparisonId });
+    })
+    .then(function(){
+      models.Criterion.destroy({ ComparisonId: comparisonId });
+    })
+    .then(function(){
+      models.Score.destroy({ ComparisonId: comparisonId });
+    })
+    .then(function(){
+      models.Vote.destroy({ ComparisonId: comparisonId });
+    })
+    .then(
+      function(){
+        res.send(204);
+      },
+      function(error){
+        res.send(500, error);
+      }
+    );    
 };
