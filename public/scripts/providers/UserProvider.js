864a7750ce8dd8b04cbdd79a0f73193248ba3d8c
Jeff Lee <leejeffc@gmail.com>
2014-02-03 00:05:32 -0800

    Clean up comments

--- public/scripts/providers/UserProvider.js
+++ public/scripts/providers/UserProvider.js
@@ -1,23 +1,11 @@
 angular.module('CoCompareApp').provider('UserProvider', function(){
   'use strict';
 
-  var userData = {};
-
-  // this.setUser = function(userFromServer){
-  //   userData = {
-  //     name: userFromServer.name,
-  //     fbId: userFromServer.fbId,
-  //     id: userFromServer.id
-  //   };
-  // };
-
-  // this.clearUser = function(){
-  //   userData = {
-  //     name: '',
-  //     fbId: -1,
-  //     id: -1
-  //   };
-  // };
+  var userData = {
+    name: '',
+    fbId: -1,
+    id: -1
+  };
 
   this.$get = function(){
     return {
